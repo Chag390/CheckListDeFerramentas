e85efc21a28f17779f1ac597dce4f99493c1c003
claudio.chag@hotmail.com <claudio.chag@hotmail.com>
2023-08-27 18:00:46 -0300

    Auterado as corres  do layout, adicionado Entry para destino, adicionado Button de QRcode e Entry, Trocado o layout da area de ''Cor do mês" de horizontalLayout pra Grid.

--- MainPage.xaml.cs
+++ MainPage.xaml.cs
@@ -7,10 +7,8 @@ public partial class MainPage : ContentPage
 	public MainPage()
 	{
 		InitializeComponent();
-        Entry entry = new Entry { Placeholder = "Enter text here" };
-        entry.Keyboard = Keyboard.Create(KeyboardFlags.Suggestions | KeyboardFlags.CapitalizeSentence);
-
         
+
     }
 
 
